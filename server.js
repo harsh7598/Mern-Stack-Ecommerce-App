a4ec24563c3a236318eb60355c57a4457de6c724
harsh7598 <harryht7@gmail.com>
2024-06-01 17:00:08 +0530

    mongoose connection

--- server.js
+++ server.js
@@ -2,18 +2,31 @@
 import  express  from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
 
 //config env file
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
 
+//middelwares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+
+
 //rest api
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome To Ecommerce App</h1>"
-    
-  );
+  res.send("<h1>Welcome To Ecommerce App</h1>");
 });
 
 //Port
